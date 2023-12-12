289152390a132ab944df516e3ff7fbabf65f3d1a
Gabriel Halus <gabrielhalus@MacBook-Pro-de-Gabriel.local>
2023-12-12 06:04:43 +0100

    feat: Implement HATEOAS links in DocumentController

--- src/controllers/SchemaController.ts
+++ src/controllers/SchemaController.ts
@@ -6,7 +6,18 @@ class SchemaController {
   async getAllSchemas(req: Request, res: Response) {
     try {
       const schemas = await schemaService.getAllSchemas();
-      res.json(schemas);
+      const schemasWithLinks = schemas.map((schema) => {
+        return {
+          ...schema._doc,
+          links: [
+            { rel: "self", href: `/schemas/${schema._id}` },
+            { rel: "update", href: `/schemas/${schema._id}`, method: "PUT" },
+            { rel: "delete", href: `/schemas/${schema._id}`, method: "DELETE" },
+            { rel: "documents", href: `/schemas/${schema._id}/documents` },
+          ]
+        }
+      })
+      res.json(schemasWithLinks);
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
@@ -19,7 +30,16 @@ class SchemaController {
       const schema = await schemaService.getSchemaById(schemaId);
       // Handle the case where schema with the given ID was not found
       if (!schema) return res.status(404).json({ error: `Schema with ID ${schemaId} not found` });
-      res.json(schema);
+      const schemaWithLinks = {
+        ...schema._doc,
+        links: [
+          { rel: "self", href: `/schemas/${schema._id}` },
+          { rel: "update", href: `/schemas/${schema._id}`, method: "PUT" },
+          { rel: "delete", href: `/schemas/${schema._id}`, method: "DELETE" },
+          { rel: "documents", href: `/schemas/${schema._id}/documents` },
+        ]
+      }
+      res.json(schemaWithLinks);
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
@@ -30,7 +50,18 @@ class SchemaController {
 
     try {
       const schemas = await schemaService.getSchemasForProject(projectId);
-      res.json(schemas);
+      const schemasWithLinks = schemas.map((schema) => {
+        return {
+          ...schema._doc,
+          links: [
+            { rel: "self", href: `/schemas/${schema._id}` },
+            { rel: "update", href: `/schemas/${schema._id}`, method: "PUT" },
+            { rel: "delete", href: `/schemas/${schema._id}`, method: "DELETE" },
+            { rel: "documents", href: `/schemas/${schema._id}/documents` },
+          ]
+        }
+      })
+      res.json(schemasWithLinks);
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
@@ -40,8 +71,16 @@ class SchemaController {
     const schemaData = req.body as ISchema;
 
     try {
-      const schema = await schemaService.createSchema(schemaData);
-      res.status(201).json(schema);
+      const newSchema = await schemaService.createSchema(schemaData);
+      const newSchemaWithLinks = {
+        ...newSchema._doc,
+        links: [
+          { rel: "self", href: `/schemas/${newSchema._id}` },
+          { rel: "update", href: `/schemas/${newSchema._id}`, method: "PUT" },
+          { rel: "delete", href: `/schemas/${newSchema._id}`, method: "DELETE" },
+        ]
+      }
+      res.status(201).json(newSchemaWithLinks);
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
@@ -55,7 +94,15 @@ class SchemaController {
       const updatedSchema = await schemaService.updateSchema(schemaId, schemaData);
       // Handle the case where schema with the given ID was not found
       if (!updatedSchema) return res.status(404).json({ error: `Schema with ID ${schemaId} not found` });
-      res.json(updatedSchema);
+      const updatedSchemaWithLinks = {
+        ...updatedSchema._doc,
+        links: [
+          { rel: "self", href: `/schemas/${updatedSchema._id}` },
+          { rel: "update", href: `/schemas/${updatedSchema._id}`, method: "PUT" },
+          { rel: "delete", href: `/schemas/${updatedSchema._id}`, method: "DELETE" },
+        ]
+      }
+      res.json(updatedSchemaWithLinks);
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
@@ -68,7 +115,10 @@ class SchemaController {
       const deletedSchema = await schemaService.deleteSchema(schemaId);
       // Handle the case where schema with the given ID was not found
       if (!deletedSchema) return res.status(404).json({ error: `Schema with ID ${schemaId} not found` });
-      res.json(deletedSchema);
+      res.json({
+        message: "Successfully deleted",
+        links: [{ rel: "create", href: `/schemas`, method: "POST" }]
+      });
     } catch (error: any) {
       res.status(500).json({ error: error.message });
     }
